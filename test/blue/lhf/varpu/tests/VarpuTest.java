1a60ad01b2ce770535a1d8f0d3730fd5dbe35ffb
bluelhf <ilari.suhonen@gmail.com>
2022-08-11 08:47:43 +0300

    chore: remove useless whitespace

--- test/blue/lhf/varpu/tests/VarpuTest.java
+++ test/blue/lhf/varpu/tests/VarpuTest.java
@@ -8,13 +8,13 @@ import static java.lang.Math.*;
 public class VarpuTest {
     public void testQuaternions() {
         assert euler(0, 0, toRadians(-45))
-            .angle(euler(0, 0, toRadians(90))) == toRadians(135) :
+            .angle(euler(0, 0, toRadians(90))) == toRadians(135):
             "Quaternion angle was incorrect";
     }
 
     public void testDistance() {
         assert ternion(-1.0, -1.0, -1.0)
-            .distance(ternion(1.0, 1.0, 1.0)) == sqrt(12) :
+            .distance(ternion(1.0, 1.0, 1.0)) == sqrt(12):
             "Ternions miscomputed distance";
     }
 }
