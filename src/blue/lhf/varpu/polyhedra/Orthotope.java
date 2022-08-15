5a615b9dd36e97c917c71a3d5698ee424d69fb25
bluelhf <ilari.suhonen@gmail.com>
2022-08-15 09:21:37 +0300

    chore: document Orthotope.java

--- src/blue/lhf/varpu/polyhedra/Orthotope.java
+++ src/blue/lhf/varpu/polyhedra/Orthotope.java
@@ -3,7 +3,16 @@ package blue.lhf.varpu.polyhedra;
 import blue.lhf.varpu.vector.*;
 
 public interface Orthotope<S, Self extends Orthotope<S, Self>> {
+    /**
+     * @return The dimension of this orthotope, i.e., how many origin-connected edges it has.
+     * */
     int dimension();
+    /**
+     * @return The origin point of this orthotope.
+     * */
     IVector<S, ?> origin();
+    /**
+     * @return The origin-connected edges of this orthotope.
+     * */
     IVector<S, ?>[] originalEdges();
 }
